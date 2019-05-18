814261cb168d38d823396b664ef644af598306f8
sushishk <kumarsushish51@gmail.com>
2019-05-18 14:32:48 +0530

    add secret file

---
+++ secret.py
@@ -0,0 +1,6 @@
+class Secret:
+    PROJECT_SECRET_KEY = 'ivj2nwdhabo25f7x*^_n8*-gw+%_dg0=2ggh%5z_r*%c7av3&*'
+    USERID_FOR_EMAIL = 'navprayas.np2000@gmail.com'
+    PASS_FOR_EMAIL = 'teamnavprayas@2k18'
+    PAYMENT_MERCHANT_KEY = 'ulwNhiAgmOgmYiBs'
+    PAYMENT_MERCHANT_ID = 'vibQhd01855522185852'
